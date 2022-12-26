ebf5482132002fdefb0448bfb6ebb25ee7d9b80a
shatakshiiii <shatakshimishra01@gmail.com>
2022-12-26 15:17:42 +0530

    Add default_python option

--- entrypoint.py
+++ entrypoint.py
@@ -26,6 +26,7 @@ def main() -> None:
         platforms = core.get_input("platforms", required=False).split(",")
         min_python = core.get_input("min_python", required=True)
         max_python = core.get_input("max_python", required=True)
+        default_python = core.get_input("default_python", required=True)
         strategies = {}
         for platform in PLATFORM_MAP:
             strategies[platform] = core.get_input(platform, required=False)
@@ -33,7 +34,6 @@ def main() -> None:
         core.debug(f"Testing strategy: {strategies}")
 
         result = []
-        default_python = KNOWN_PYTHONS[KNOWN_PYTHONS.index(min_python)]
         if max_python == "3.12":
             python_names = KNOWN_PYTHONS[KNOWN_PYTHONS.index(min_python) :]
         else:
@@ -89,6 +89,7 @@ if __name__ == "__main__":
         os.environ["INPUT_OTHER_NAMES"] = "lint\npkg"
         os.environ["INPUT_MIN_PYTHON"] = "3.8"
         os.environ["INPUT_MAX_PYTHON"] = "3.12"
+        os.environ["INPUT_DEFAULT_PYTHON"] = "3.10"
         os.environ["INPUT_PLATFORMS"] = "linux,macos"  # macos and windows
         os.environ["INPUT_LINUX"] = "full"
         os.environ["INPUT_MACOS"] = "minmax"
